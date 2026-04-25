61c37e8a8141a256f5bf3b6e69686d32074eaaf2
Prasanna20-jpg <prasanna20.gali@gmail.com>
2026-04-25 23:34:19 +0530

    Add files via upload

---
+++ network_delay_time.py
@@ -0,0 +1,23 @@
+import heapq
+class Solution:
+    def networkDelayTime(times,n,k):
+        adj=[[] for _ in range(n+1)]
+        for u,v,w in times:
+            adj[u].append(v,w)
+        dist=[float('inf')]*(n+1)
+        dist[k]=0
+        pq=[(0,k)]
+        while pq:
+            time,node=heapq.heappop(pq)
+            if time>dist[node]:
+                continue
+            for nei,wt in adj[node]:
+                if time+wt<dist[nei]:
+                    dist[nei]=time+wt
+                    heapq.heappush(pq,(dist[nei],nei))
+        max_time=max(dist[1:])
+        if max_time != float('inf'):
+            return max_time
+        else:
+            return -1
+        
