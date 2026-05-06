ae9285b1a2ac3a5ab4c0f6c5303bee245726e9a7
Prasanna20-jpg <prasanna20.gali@gmail.com>
2026-05-06 23:38:30 +0530

    Add files via upload

---
+++ design_linked_list.py
@@ -0,0 +1,75 @@
+class ListNode:
+    def __init__(self,val=0):
+        self.val=val
+        self.next=None
+
+class MyLinkedList:
+
+    def __init__(self):
+        self.head=None
+        self.size=0
+
+    def get(self, index: int) -> int:
+        if index<0 or index>=self.size:
+            return -1
+
+        temp=self.head
+        for _ in range(index):
+            temp=temp.next
+        return temp.val
+
+
+    def addAtHead(self, val: int) -> None:
+        newNode=ListNode(val)
+        newNode.next=self.head
+        self.head=newNode
+        self.size +=1
+
+    def addAtTail(self, val: int) -> None:
+        newNode=ListNode(val)
+        if not self.head:
+            self.head=newNode
+        else:
+            temp=self.head
+            while temp.next:
+                temp=temp.next
+            temp.next=newNode
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size:
+            return
+        
+        if index == 0:
+            self.addAtHead(val)
+            return
+        newNode = ListNode(val)
+        temp = self.head
+        for _ in range(index - 1):
+            temp = temp.next
+        newNode.next = temp.next
+        temp.next = newNode
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index<0 or index>=self.size:
+            return 
+        if index == 0:
+            self.head=self.head.next
+        else:
+            temp=self.head
+            for _ in range(index-1):
+                temp=temp.next
+            temp.next = temp.next.next
+
+        self.size -=1
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
