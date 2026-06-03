8b6ad14f53696ddf653466866325416c80568de0
Prasanna20-jpg <prasanna20.gali@gmail.com>
2026-06-03 11:36:36 +0530

    Add files via upload

---
+++ max_count_of_pos_integer_and_negative_integer.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        positive,negative=0,0
+        for i in range(len(nums)):
+            if nums[i]>0:
+                positive+=1
+            elif nums[i]<0:
+                negative+=1
+        return max(positive,negative)
