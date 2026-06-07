3be7e9842d0353786b82de54ea8df18b967180b7
Prasanna20-jpg <prasanna20.gali@gmail.com>
2026-06-07 18:44:07 +0530

    Add files via upload

---
+++ summary_ranges.py
@@ -0,0 +1,17 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        n=len(nums)
+        res=[]
+        i=0
+        while i<n:
+            start=nums[i]
+            while i+1<n and nums[i+1]==nums[i]+1:
+                i+=1
+            end=nums[i]
+            if start==end:
+                res.append(str(start))
+            else:
+                res.append(f"{start}->{end}")
+            i+=1
+        return res
+
